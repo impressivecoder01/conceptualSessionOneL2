91c414a52a7063df850bba57da753b9256a372ea
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-03 21:57:05 +0600

    des

--- primitive.ts
+++ primitive.ts
@@ -16,3 +16,27 @@ const products: {
     category: 'ele',
     discount: 444
 }
+
+// void
+
+function calculation(price: number, quantity: number): number{
+    return price * quantity
+}
+
+console.log(calculation(44,4)); // 176
+
+function totalCard(...prices: number[]): number{
+    return prices.reduce((sum,p)=> sum + p, 250)
+}
+const bestProducts = ['oil', 'salt', 'rice']
+const newProducts = [...bestProducts, 'mustard oil']
+console.log(newProducts)
+
+const buyer = {
+    nam: 'karim',
+    address: 'dhaka',
+    contactNumber: 231312323
+}
+
+const {nam,address,contactNumber} = buyer
+console.log(nam,address,contactNumber)
