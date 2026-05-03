63310087085e25a07bbc86478d60cebf6c079a3e
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-03 21:38:06 +0600

    first commit

---
+++ primitive.ts
@@ -0,0 +1,18 @@
+let shopName : string = 'Rahim Store'
+let totalProducts: number = 50;
+let isOpen: boolean = true;
+let rating: null = null;
+let discount: undefined = undefined;
+
+const products: {
+    shopName: string;
+    price: number;
+    category: 'gorocery' | 'ele'
+    discount?: number
+
+}={
+    shopName: 'sss',
+    price: 44,
+    category: 'ele',
+    discount: 444
+}
